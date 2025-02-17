dfbfda1f176f6e3ffc1e602abbe669d5619111c3
andarie1 <elena.arhi88@gmail.com>
2025-02-17 15:48:39 +0200

    added class customer

--- store_app/admin.py
+++ store_app/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from store_app.models import Category
 
+# Задание 3.1: Настройка модели Category в админке
+# Зарегистрируйте модель Category в админке с использованием декоратора @admin.register.
+# Создайте класс CategoryAdmin, чтобы настроить отображение модели в админке:
+# Установите отображение поля name в списке записей.
+# Добавьте возможность поиска по полю name.
+# Установите сортировку записей по полю name в алфавитном порядке.
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
 
