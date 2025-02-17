5e3e3e64f365a817041b5aea89a94d1b49eee457
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-02-17 13:54:05 +0100

    new/Order and Address Models

--- store_app/models.py
+++ store_app/models.py
@@ -23,3 +23,23 @@ class Supplier(models.Model):
     def __str__(self):
         return self.name
 
+
+class Address(models.Model):
+    country = models.CharField(max_length=100)
+    city = models.CharField()
+    street = models.CharField()
+    house = models.CharField()
+
+    def __str__(self):
+        return f"{self.street}, {self.house}"
+
+class Order(models.Model):
+    order_date = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey('Customer', on_delete=models.PROTECT, related_name='orders')
+
+    def __str__(self):
+        return f"{self.id} by {self.customer}"
+
+    class Meta:
+        ordering = ['-order_date']
+        get_latest_by = ['order_date']
