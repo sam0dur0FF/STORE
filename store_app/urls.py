e40acfd8484aa49a82b076f0b072975a304fe283
Danil <samodurOFF@users.noreply.github.com>
2025-03-14 16:17:42 +0500

    was added categories and address routers

--- store_app/urls.py
+++ store_app/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from store_app.views.address import AddressView
+from store_app.views.category import CategoryView
 from store_app.views.order_items import OrderItemViewSet
 from store_app.views.product_detail_views import ProductDetailView
 from store_app.views.products import ProductViewSet
 
 router = DefaultRouter()
-# router.register(r'categories', )
+router.register(r'categories', CategoryView)
 # router.register(r'suppliers', )
 router.register(r'products', ProductViewSet)
 router.register(r'products_details', ProductDetailView)
 router.register(r'order_items', OrderItemViewSet)
+router.register(r'address', AddressView)
 
 urlpatterns = [
     path('', include(router.urls)),
