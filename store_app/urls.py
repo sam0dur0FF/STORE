8fc39c9bd281e5307835ff1cc0fb0106f7d9093a
Danil <samodurOFF@users.noreply.github.com>
2025-03-14 16:11:04 +0500

    add routing

--- store_app/urls.py
+++ store_app/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-
+from store_app.views.order_items import OrderItemViewSet
+from store_app.views.product_detail_views import ProductDetailView
+from store_app.views.products import ProductViewSet
 
 router = DefaultRouter()
-router.register(r'categories', )
-router.register(r'suppliers', )
-router.register(r'products', )
+# router.register(r'categories', )
+# router.register(r'suppliers', )
+router.register(r'products', ProductViewSet)
+router.register(r'products_details', ProductDetailView)
+router.register(r'order_items', OrderItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
