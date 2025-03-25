e620eee2535d0345a5aa6a2db70e9d294cdd497e
andarie1 <elena.arhi88@gmail.com>
2025-03-25 14:58:46 +0200

    new routers added

--- store_app/urls.py
+++ store_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from store_app.views.address import AddressView
 from store_app.views.category import CategoryView
 from store_app.views.customer_views import CustomerView
+from store_app.views.order import OrderViewSet
 from store_app.views.order_items import OrderItemViewSet
 from store_app.views.product_detail_views import ProductDetailView
 from store_app.views.products import ProductViewSet
+from store_app.views.supplier_views import SupplierViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryView)
@@ -15,7 +16,10 @@ router.register(r'products', ProductViewSet)
 router.register(r'products_details', ProductDetailView)
 router.register(r'order_items', OrderItemViewSet)
 router.register(r'addresses', AddressView)
+router.register(r'orders', OrderViewSet)
+router.register(r'suppliers', SupplierViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
