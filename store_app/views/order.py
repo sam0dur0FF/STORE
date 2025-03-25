c7c5a2975bec9922083c46543198d265ca4f52b3
Danil <samodurOFF@users.noreply.github.com>
2025-03-25 18:03:31 +0500

    fixed SupplierViewSet and OrderViewSet

--- store_app/views/order.py
+++ store_app/views/order.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
-from store_app.models import OrderItem
+from store_app.models import Order
 from store_app.serializers import OrderSerializer, OrderCreateUpdateSerializer
 
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = OrderItem.objects.all()
+    queryset = Order.objects.all()
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
--- store_app/views/supplier_views.py
+++ store_app/views/supplier_views.py
@@ -1,10 +1,10 @@
-from rest_framework import status, viewsets, mixins
-from store_app.models import ProductDetail
+from rest_framework import viewsets
+from store_app.models import Supplier
 from store_app.serializers import SupplierSerializer
 
 
 class SupplierViewSet(viewsets.ModelViewSet):
-    queryset = ProductDetail.objects.all()
+    queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
 
 
