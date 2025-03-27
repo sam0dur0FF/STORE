b505e7661b5ca6501f411148c076641388373bb7
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-03-27 14:37:57 +0200

    modified:   store_app/views/product_detail_views.py 	modified:   store_app/views/supplier_views.py

--- store_app/views/product_detail_views.py
+++ store_app/views/product_detail_views.py
@@ -1,10 +1,13 @@
 from rest_framework import status, viewsets, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from store_app.models import ProductDetail
 from store_app.serializers import ProductDetailSerializer, ProductDetailCreateUpdateSerializer
 
 
 class ProductDetailView(viewsets.ModelViewSet):
     queryset = ProductDetail.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
     def get_serializer_class(self):
