60dce4a7a973864bd600e437ed84b3b2d61f724f
AndreyLapko1 <andreycgo765@gmail.com>
2025-03-31 18:04:20 +0300

    fix

--- STORE/settings.py
+++ STORE/settings.py
@@ -120,7 +120,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     )
+# }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
