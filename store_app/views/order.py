b7f25e089bcc72a4d33618a4237b19fb9edacb78
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-03-31 13:57:04 +0300

    added IsCustomerOrReadOnly to OrderViewSet

--- store_app/views/order.py
+++ store_app/views/order.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 from store_app.models import Order
+from store_app.permissions import IsCustomerOrReadOnly
 from store_app.serializers import OrderSerializer, OrderCreateUpdateSerializer
 
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
+    pagination_class = [IsCustomerOrReadOnly]
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
